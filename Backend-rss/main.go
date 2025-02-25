5f2b0994678010b78d325d3f72de1c46a683fe15
Color-Kat <katik7979@gmail.com>
2025-02-25 22:45:53 +0300

    my first go backend server

--- Backend-rss/main.go
+++ Backend-rss/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("hi")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("PORT is not found in the environment")
+		return
+	}
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "https://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Println(".Server is running on port " + portString)
+	err = server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
