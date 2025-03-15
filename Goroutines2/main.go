ecc081210c84e0d655e9ed3cf5543766a48f16a9
Color-Kat <katik7979@gmail.com>
2025-03-15 12:18:23 +0300

    array sum using goroutines

--- Goroutines2/main.go
+++ Goroutines2/main.go
@@ -34,16 +34,60 @@ func requestChannel(codeCh chan int) {
 
 func channel() {
 	codeCh := make(chan int)
+	var wg sync.WaitGroup
+
 	for i := 0; i < 10; i++ {
-		go requestChannel(codeCh)
+		wg.Add(1)
+
+		go func() {
+			requestChannel(codeCh)
+			wg.Done()
+		}()
 	}
+
+	go func() {
+		wg.Wait()
+		close(codeCh)
+	}()
+
 	for res := range codeCh {
 		fmt.Println(res)
 	}
 }
 
+func sumArr(arr []int, sumCh chan int) {
+	sum := 0
+	for _, number := range arr {
+		sum += number
+	}
+	sumCh <- sum
+}
+
+func sliceSumExample() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+	numGoroutines := 3
+	chunkSize := len(arr) / numGoroutines
+
+	sumCh := make(chan int, numGoroutines)
+
+	for i := 0; i < numGoroutines; i++ {
+		start := i * chunkSize
+		end := start + chunkSize
+		fmt.Println(end)
+
+		go sumArr(arr[start:end], sumCh)
+	}
+
+	totalSum := 0
+	for i := 0; i < numGoroutines; i++ {
+		totalSum += <-sumCh
+	}
+	fmt.Println(totalSum)
+
+}
+
 func main() {
 	//waitGroup()
-
-	channel()
+	//channel()
+	sliceSumExample()
 }
