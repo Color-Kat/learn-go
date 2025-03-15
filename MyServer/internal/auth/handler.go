233a4e95b3e3dfeb7bf699c0d048c90f41264c61
Color-Kat <katik7979@gmail.com>
2025-03-15 21:00:33 +0300

    primitive validation

--- MyServer/internal/auth/handler.go
+++ MyServer/internal/auth/handler.go
@@ -3,8 +3,10 @@ package auth
 import (
 	"demo/http/configs"
 	"demo/http/pkg/response"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"regexp"
 )
 
 type AuthHandlerDeps struct {
@@ -24,7 +26,24 @@ func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
 
 func (handler *AuthHandler) Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		fmt.Println("login")
+		var payload LoginRequest
+		err := json.NewDecoder(req.Body).Decode(&payload)
+		if err != nil {
+			response.Json(w, err.Error(), 402)
+		}
+
+		// Validation
+		if payload.Email == "" || payload.Password == "" {
+			response.Json(w, "Email and Password required", 402)
+			return
+		}
+
+		match, _ := regexp.MatchString(`[\w\.%+\-]+@[\w\.+\-]+\.[A-Za-z]{2,}`, payload.Email)
+		if !match {
+			response.Json(w, "Wrong Email", 402)
+			return
+		}
+
 		res := LoginResponse{
 			Token: handler.Config.Auth.SecretToken,
 		}
