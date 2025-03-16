3c43c67908980a1e343eb0d6ba272985e86108dd
Color-Kat <katik7979@gmail.com>
2025-03-16 17:07:42 +0300

    delete links

--- MyServer/internal/link/handler.go
+++ MyServer/internal/link/handler.go
@@ -3,8 +3,9 @@ package link
 import (
 	"demo/http/pkg/request"
 	"demo/http/pkg/response"
-	"fmt"
+	"gorm.io/gorm"
 	"net/http"
+	"strconv"
 )
 
 type LinkHandlerDeps struct {
@@ -24,6 +25,19 @@ func NewLinkHandler(router *http.ServeMux, deps LinkHandlerDeps) {
 	router.HandleFunc("GET /{hash}", handler.GoTo())
 }
 
+func (handler *LinkHandler) GoTo() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		hash := req.PathValue("hash")
+		link, err := handler.LinkRepository.GetByHash(hash)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+
+		http.Redirect(w, req, link.Url, http.StatusTemporaryRedirect)
+	}
+}
+
 func (handler *LinkHandler) Create() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		body, err := request.HandleBody[LinkCreateRequest](&w, req)
@@ -32,9 +46,20 @@ func (handler *LinkHandler) Create() http.HandlerFunc {
 		}
 
 		link := NewLink(body.Url)
+
+		for {
+			existedLink, _ := handler.LinkRepository.GetByHash(link.Hash)
+			if existedLink == nil {
+				break
+			} else {
+				link.GenerateHash()
+			}
+		}
+
 		createdLink, err := handler.LinkRepository.Create(link)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
 		}
 
 		response.Json(w, createdLink, http.StatusCreated)
@@ -43,23 +68,49 @@ func (handler *LinkHandler) Create() http.HandlerFunc {
 
 func (handler *LinkHandler) Update() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		id := req.PathValue("id")
-		fmt.Println(id)
+		body, err := request.HandleBody[LinkUpdateRequest](&w, req)
+		if err != nil {
+			return
+		}
+		id, err := strconv.Atoi(req.PathValue("id"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
+		link, err := handler.LinkRepository.Update(&Link{
+			Model: gorm.Model{ID: uint(id)},
+			Url:   body.Url,
+			Hash:  body.Hash,
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		response.Json(w, link, http.StatusCreated)
 	}
 }
 
 func (handler *LinkHandler) Delete() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		id := req.PathValue("id")
-		fmt.Println(id)
+		id, err := strconv.Atoi(req.PathValue("id"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-	}
-}
+		_, err = handler.LinkRepository.GetById(uint(id))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
 
-func (handler *LinkHandler) GoTo() http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-		hash := req.PathValue("hash")
-		fmt.Println(hash)
+		err = handler.LinkRepository.Delete(uint(id))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		response.Json(w, nil, 200)
 	}
 }
--- MyServer/internal/link/repository.go
+++ MyServer/internal/link/repository.go
@@ -2,6 +2,7 @@ package link
 
 import (
 	"demo/http/pkg/database"
+	"gorm.io/gorm/clause"
 )
 
 type LinkRepository struct {
@@ -20,4 +21,38 @@ func (repo *LinkRepository) Create(link *Link) (*Link, error) {
 		return nil, result.Error
 	}
 	return link, nil
+}
+
+func (repo *LinkRepository) GetByHash(hash string) (*Link, error) {
+	var link Link
+	result := repo.Database.First(&link, "hash = ?", hash)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &link, nil
+}
+
+func (repo *LinkRepository) GetById(id uint) (*Link, error) {
+	var link Link
+	result := repo.Database.First(&link, "id = ?", id)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &link, nil
+}
+
+func (repo *LinkRepository) Update(link *Link) (*Link, error) {
+	result := repo.Database.Clauses(clause.Returning{}).Updates(link)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return link, nil
+}
+
+func (repo *LinkRepository) Delete(id uint) error {
+	result := repo.Database.Delete(&Link{}, id)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
 }
