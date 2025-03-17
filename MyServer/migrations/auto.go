d231436eb467346f7e150f9a1166b9971c677dfe
Color-Kat <katik7979@gmail.com>
2025-03-17 21:37:43 +0300

    user model and user repository

--- MyServer/migrations/auto.go
+++ MyServer/migrations/auto.go
@@ -3,12 +3,16 @@ package main
 import (
 	"demo/http/configs"
 	"demo/http/internal/link"
-	"demo/http/pkg/db"
+	"demo/http/internal/user"
+	"demo/http/pkg/database"
 )
 
 func main() {
 	config := configs.LoadConfig()
 	database := database.NewDB(config)
 
-	database.AutoMigrate(&link.Link{})
+	database.AutoMigrate(
+		&link.Link{},
+		&user.User{},
+	)
 }
